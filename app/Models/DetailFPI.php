c34603c7c3002a21e96790c17f3e45d374851a07
mbakama <131680152+mbakama@users.noreply.github.com>
2023-05-09 10:36:47 +0100

    version 2.3.3

--- app/Models/DetailFPI.php
+++ app/Models/DetailFPI.php
@@ -5,7 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class DetailFPI extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['numero',
+'CodeSource',
+'MontantCreditFc',
+'MontantCreditUSD',
+'Promoteur',
+'AdressPromoteur',
+'observation',
+'Annee',
+'CoNum',
+'DateCreation',
+'Status'];
 }
--- database/migrations/2023_05_08_124229_create_phones_table.php
+++ database/migrations/2023_05_08_124229_create_phones_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('phones', function (Blueprint $table) {
             $table->id();
             $table->string('telephone');
-            $table->foreignId('detailfpi_id')->constrained();
+            
             $table->timestamps();
         });
     }
